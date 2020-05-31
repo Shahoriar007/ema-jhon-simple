40b521ea0e392f11d286b97e2321052a1ade3bb4
Shahoriar <shahoriar.fahim@gmail.com>
2020-05-31 15:25:07 -0700

    Routeing Basic

---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h3>Sorry not found</h3>
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default Notfound;
