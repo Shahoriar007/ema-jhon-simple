38661cbe305929ecec0a3004f1522b81ae1e4a6f
Shahoriar <shahoriar.fahim@gmail.com>
2020-06-05 16:39:26 -0700

    Place order button done!

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -38,9 +38,9 @@ const Cart = (props) => {
             <p><small>Tax: {tax}</small></p>
             <p>Total Price : {total + shipping + tax}</p>
             <br/>
-            <Link to="/review">
-                <button className="main-button" >Review Order</button>
-            </Link>
+            {
+                props.children
+            }
         </div>
     );
 };
