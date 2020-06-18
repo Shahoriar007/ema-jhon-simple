50c52d6d23ed881d20f35c73a8424bb8184a56ce
Shahoriar <shahoriar.fahim@gmail.com>
2020-06-17 19:34:19 -0700

    Sign in with google Button Done!

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Auth from './use-auth';
 
 const Login = () => {
+    const auth = Auth();
+    console.log(auth.user);
     return (
         <div>
             <h1>Wellcome to login</h1>
+            {
+                auth.user ? <button>Sign Out</button> :
+                <button onClick={auth.signInWithGoogle}>Sign In with google</button>
+            }
+            
         </div>
     );
 };
--- src/components/Login/use-auth.js
+++ src/components/Login/use-auth.js
@@ -1,6 +1,34 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig form "../../firebase.config";
+import firebaseConfig from "../../firebase.config";
+import { useState } from "react";
 
 
 firebase.initializeApp(firebaseConfig);
+
+const Auth = () => {
+    const [user, setUser] = useState(null);
+
+    const signInWithGoogle = () =>{
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+        .then(res => {
+            const {displayName, email, photoURL} = res.user;
+            const signedInUser = {name: displayName, email, photo: photoURL};
+            setUser(signedInUser);
+            return res.user;
+        })
+        .catch(err => {
+            console.log(err);
+            setUser(null);
+            return err.mesage;
+        })
+    }
+    return{
+        user,
+        signInWithGoogle
+    }
+
+}
+
+export default Auth;
